43959a63e60eac3f032c65e676b5d3421f8ab951
David Rosson <david@rosson.com.au>
2015-05-22 14:06:02 +1000

    stringified

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,11 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
+var getElementsByClassName = function(className){
   // your code here
 };
+
+
+document.body
+element.childNodes
+element.classList
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,52 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  return process(obj);
 };
+
+var process = function(item) {
+
+    // deal with null
+    if ( item === null ) { return 'null'; }
+
+    var t = typeof item;
+    // return primitive types
+    if ( t === 'string' ) { return '\"' + item + '\"'; }
+    if ( t === 'boolean' || t === 'number' ) { return item.toString(); }
+
+    // unwrap array
+    if ( item.constructor === Array ) {
+        // start an empty array
+        var outArr = '[';
+        // process each item in the array
+        for (var i=0; i<item.length; i++) {
+            if (i>0) { outArr += ','; }
+            outArr = outArr + process(item[i]);
+        }
+        outArr += ']';
+        return outArr;
+    }
+
+    // unwrap hash
+    if ( item.constructor === Object ) {
+        var outObj = '{',
+            first = true;
+        for (var key in item) {
+            // skip functions and undefined
+            if (typeof item[key] === ('function' || 'undefined')) {
+                return '{}';
+            }
+            if ( !first ) { outObj += ','; }
+            first = false;
+            outObj += '\"' + key + '\":';
+            outObj = outObj + process(item[key]);
+        }
+        outObj += '}';
+        return outObj;
+    }
+
+    // catch all oops
+    return '{}';
+
+};
+
