a54940d8567530bea3e0fe7cb70cef1e13c3358c
David Rosson <david@rosson.com.au>
2015-05-22 16:47:48 +1000

    dom traversal

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -5,10 +5,28 @@
 
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
-  // your code here
+    // your code here
+    outArr = [];
+    explore(document.body, className);
+    return outArr;
 };
 
+var explore = function(node, selector, accum){
 
-document.body
-element.childNodes
-element.classList
+    checkClass(node, selector);
+
+    for (var i=0; i<node.childNodes.length; i++) {
+        checkClass(node.childNodes[i], selector);
+        if (node.childNodes[i].childElementCount) {
+            explore(node.childNodes[i], selector);
+        }
+    }
+};
+
+var checkClass = function(node, selector){
+    if ( node.classList ) {
+        if ( node.classList.contains(selector) ) {
+            outArr.push(node);
+        }
+    }
+};
